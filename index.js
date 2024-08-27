b624771ca94d3383bc56fe094a0a2a4d95d86363
Muhammad Aleem <aawais12357@gmail.com>
2024-08-27 16:03:41 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 // Node.js program to clear clutter inside of a directory and organize the contents of that directory into different folders
 
 
-import fs from "fs/promises"
-import fsn from "fs"
-import path from "path"
-//Put Base path here first
-const basepath = "D:\\practice\\clearclutter"
+import fs from "fs/promises";
+import fsn from "fs";
+import path from "path";
 
-let files = await fs.readdir(basepath)
+// Check if a base path is provided as a command-line argument
+if (process.argv.length < 3) {
+  console.log("Please provide the base path as a command-line argument.");
+  process.exit(1);
+}
+
+const basepath = process.argv[2];
+
+let files = await fs.readdir(basepath);
 
 for (const item of files) {
-    console.log("running for ", item)
-    let ext = item.split(".")[item.split(".").length - 1]
-    if (ext != "js" && ext != "json" && item.split(".").length > 1) {
-
-
-        if (fsn.existsSync(path.join(basepath, ext))) {
-            // Move the file to this directory if its not a js or json file
-            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
-        }
-        else {
-            fs.mkdir(ext)
-            fs.rename(path.join(basepath, item), path.join(basepath, ext, item))
-        }
+  console.log("Running for", item);
+  let ext = item.split(".")[item.split(".").length - 1];
+  if (ext !== "js" && ext !== "json" && item.split(".").length > 1) {
+    if (fsn.existsSync(path.join(basepath, ext))) {
+      // Move the file to this directory if it's not a .js or .json file
+      await fs.rename(path.join(basepath, item), path.join(basepath, ext, item));
+    } else {
+      await fs.mkdir(path.join(basepath, ext));
+      await fs.rename(path.join(basepath, item), path.join(basepath, ext, item));
     }
-
+  }
 }
